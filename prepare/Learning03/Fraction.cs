122f7d0a87d9bea937bcde9a799a7dd133ed6e9f
sayraarriaga <sayraarriaga4@gmail.com>
2023-05-03 02:40:43 -0500

    LEarning activity w03 Fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,28 @@
+using System;
+public class Fraction{
+private int _top;
+private int _bottom;
+ public Fraction(){
+    _top = 1;
+    _bottom = 1;
+    
+ }
+ public Fraction(int WholeNumber){
+    _top= WholeNumber;
+    _bottom =1;
+ }
+ public Fraction(int top, int bottom){
+ _top = top;
+ _bottom = bottom;
+ }
+ public string GetFractionString(){
+    string fraction1 = $"{_top}/{_bottom}";
+    return fraction1;
+
+ }
+ public double GetDecimalValue(){
+    
+    return (double)_top / (double)_bottom;
+ }
+
+}
